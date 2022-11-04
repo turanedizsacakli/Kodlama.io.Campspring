2a6fce653a38fd0d2d78ed6d8b484eead332400d
TURAN EDİZ SAÇAKLI <turanedizsacakli@yahoo.com.tr>
2022-11-04 03:35:04 +0300

    ...

---
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,55 @@
+package kodlama.io.rentACar.business.concretes;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+
+@Service//bu sınıf bir business nesnesidir...
+public class BrandManager implements BrandService{
+	
+	private BrandRepository brandRepository;
+
+	@Autowired
+	public BrandManager(BrandRepository brandRepository) {
+		super();
+		this.brandRepository = brandRepository;
+	}
+
+	@Override
+	public List<GetAllBrandResponse> getAll() {
+		// iş kuralları 
+		List<Brand> brands= brandRepository.findAll();
+		List<GetAllBrandResponse> brandResponse =new ArrayList<GetAllBrandResponse>();
+		
+		for (Brand brand : brands) {
+			GetAllBrandResponse responseItem=new GetAllBrandResponse();
+			responseItem.setId(brand.getId());
+			responseItem.setName(brand.getName());
+			brandResponse.add(responseItem);
+			
+		}
+		//return brandRepository.getAll();
+		return brandResponse;
+	}
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		Brand brand =new Brand();
+		brand.setName(createBrandRequest.getName());
+		
+		this.brandRepository.save(brand);
+		
+	}
+	
+	
+}
